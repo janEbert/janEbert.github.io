b9312bedb7234d27dbc357213914f09318a03a47
janEbert <janpublicebert@posteo.net>
2020-09-20 00:31:40 +0200

    Improve make script performance
    Also improve comments and structure using checkdoc.

--- make.el
+++ make.el
@@ -1,5 +1,18 @@
-(let ((org-confirm-babel-evaluate nil))
+;;; make.el --- Build script for the website  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Script to build the website.
+
+(require 'ob-core)
+;; Not required but do it just in case.
+(require 'files)
+
+;;; Code:
+
+(let ((org-confirm-babel-evaluate nil)
+	  (make-backup-files nil)
+	  (gc-cons-threshold 402653184)
+	  (gc-cons-percentage 0.6))
   (org-babel-load-file "publish.org"))
 
-(provide 'make)
 ;;; make.el ends here
